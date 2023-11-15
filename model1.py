7129b096da04bd8828a53566f626c2c1a4265f5e
chyobin <chyobin17@gmail.com>
2023-11-16 08:50:53 +0900

    Rb

--- model1.py
+++ model1.py
@@ -3,41 +3,37 @@ from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
 import time
-import threading
+import random
 
 # Load the model
 model = load_model("keras_Model.h5", compile=False)
 
-# Load the labels
-class_names = open("labels.txt", "r").readlines()
+# Load the labels with UTF-8 encoding
+class_names = open("labels.txt", "r", encoding="utf-8").readlines()
 
 # Create the array of the right shape to feed into the keras model
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Open a connection to the camera (0 is usually the default camera)
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # 원하는 너비
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # 원하는 높이
 
 # Variables for action detection
-start_time = None
+start_time = time.time()
+action_interval = 5  # The interval (in seconds) to detect the action
 action_detected = False
-action_threshold = 5  # The duration (in seconds) to detect the action
-frame_skip = 3  # Process every 3rd frame
-frame_count = 0
 
-# Function to read frames from the camera
-def read_camera():
-    global frame_count
-    while True:
-        ret, frame = cap.read()
-        if frame_count % frame_skip == 0:
-            process_frame(frame)
-        frame_count += 1
+# Rock-paper-scissors game variables
+user_choice = None
+computer_choice = None
+result = None
 
-# Function to process each frame
-def process_frame(frame):
-    global action_detected, start_time
+def get_computer_choice():
+    return random.choice(["Rock", "Paper", "Scissors"])
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
     # Convert the frame to PIL Image
     image = Image.fromarray(frame)
 
@@ -57,29 +53,44 @@ def process_frame(frame):
     # Display the frame with prediction
     cv2.putText(frame, f"Class: {class_name[2:]} - Confidence: {confidence_score:.2f}", (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow('Action Detection', frame)
+    cv2.imshow('Rock-Paper-Scissors Game', frame)
 
     # Check for action detection
-    if confidence_score > 0.5:  # Adjust the confidence threshold as needed
-        if not action_detected:
-            action_detected = True
-            start_time = time.time()
-    else:
-        action_detected = False
+    elapsed_time = time.time() - start_time
+    remaining_time = action_interval - elapsed_time
 
-    # Check if action has been detected for the specified duration
-    if action_detected and (time.time() - start_time) > action_threshold:
-        print("Action Detected!")
-        # Reset variables to avoid continuous detection for the same action
-        action_detected = False
-        start_time = None
+    if confidence_score > 0.5 and elapsed_time >= action_interval:
+        print(f"Action Detected! Time: {elapsed_time:.2f} seconds")
+        
+        # Map class names to rock, paper, or scissors
+        if "Rock" in class_name:
+            user_choice = "Rock"
+        elif "Paper" in class_name:
+            user_choice = "Paper"
+        elif "Scissors" in class_name:
+            user_choice = "Scissors"
 
-# Start camera reading thread
-camera_thread = threading.Thread(target=read_camera)
-camera_thread.start()
+        # Get computer's choice
+        computer_choice = get_computer_choice()
 
-# Main loop
-while True:
+        # Determine the game result
+        if user_choice == computer_choice:
+            result = "It's a tie!"
+        elif (
+            (user_choice == "Rock" and computer_choice == "Scissors") or
+            (user_choice == "Paper" and computer_choice == "Rock") or
+            (user_choice == "Scissors" and computer_choice == "Paper")
+        ):
+            result = "You win!"
+        else:
+            result = "You lose!"
+
+        # Print and reset variables for the next detection
+        print(f"User Choice: {user_choice}, Computer Choice: {computer_choice}")
+        print(result)
+        action_detected = True
+        start_time = time.time()
+
     # Break the loop if 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -87,4 +98,5 @@ while True:
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
 
