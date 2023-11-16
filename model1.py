696c498282de200e501cd115cad73ab7c59150f2
chyobin <chyobin17@gmail.com>
2023-11-16 15:08:08 +0900

    Rb

--- model1.py
+++ model1.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template
 import cv2
 from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
 import time
 import random
-
-app = Flask(__name__)
 
 # Load the model
 model = load_model("keras_Model.h5", compile=False)
@@ -45,27 +42,10 @@ def provide_hint():
         "이 인물은 대중 매체에 자주 등장하는 연예인입니다."
     ]
     if hints_given < len(hints):
-        hint = hints[hints_given]
+        print("힌트: " + hints[hints_given])
         hints_given += 1
-    else:
-        hint = "힌트를 모두 사용했습니다."
-    return hint
 
-# Route for the home page
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# Route for the game page
-@app.route('/game')
-def game():
-    return render_template('game.html')
-
-# Route for processing game actions
-@app.route('/game/action', methods=['POST'])
-def game_action():
-    global user_choice, computer_choice, result, start_time, action_interval, hints_given, rounds_played, cap
-
+while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -85,11 +65,22 @@ def game_action():
     class_name = class_names[index]
     confidence_score = prediction[0][index]
 
-    # Check for action detection
+    # Display the frame with prediction
+    cv2.putText(frame, f"Class: {class_name[2:]} - Confidence: {confidence_score:.2f}", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    # Calculate and display the remaining time
     elapsed_time = time.time() - start_time
     remaining_time = max(0, action_interval - elapsed_time)
+    cv2.putText(frame, f"Remaining Time: {remaining_time:.2f} seconds", (10, 70),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    cv2.imshow('Rock-Paper-Scissors Game', frame)
 
+    # Check for action detection
     if confidence_score > 0.5 and elapsed_time >= action_interval:
+        print(f"Action Detected! Time: {elapsed_time:.2f} seconds")
+        
         # Map class names to rock, paper, or scissors
         if "Rock" in class_name:
             user_choice = "Rock"
@@ -109,11 +100,14 @@ def game_action():
             (user_choice == "Paper" and computer_choice == "Rock") or
             (user_choice == "Scissors" and computer_choice == "Paper")
         ):
-            result = "이겼습니다! " + provide_hint()  # Provide a hint upon winning
+            result = "이겼습니다! "
+            provide_hint()  # Provide a hint upon winning
         else:
             result = "이런~ 지셨군요. 다시 도전해 보세요!"
 
-        # Reset variables for the next detection
+        # Print and reset variables for the next detection
+        print(f"User Choice: {user_choice}, Computer Choice: {computer_choice}")
+        print(result)
         action_detected = True
         start_time = time.time()
         hints_given = 0
@@ -121,11 +115,23 @@ def game_action():
 
         # Check if 5 rounds have been played
         if rounds_played == 5:
-            result = "5판이 끝났습니다. 이제 김연아의 인물을 맞춰보세요!"
+            print("5판이 끝났습니다. 이제 김연아의 인물을 맞춰보세요!")
+            break
 
-    return render_template('game.html', user_choice=user_choice, computer_choice=computer_choice,
-                           result=result, remaining_time=remaining_time)
+    # Break the loop if 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Ask the user to guess the person
+print("기회가 끝났습니다. 이 사람은 누굴까요?")
+user_guess = input("인물의 이름을 입력하세요: ")
 
+# Check if the user's guess is correct
+if user_guess == "김연아":
+    print("정답입니다! 축하합니다!")
+else:
+    print("아쉽군요. 다시 도전해 보세요!")
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
