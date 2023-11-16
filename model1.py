5b8b4cffc01e7656bab375c92577973c4954d833
chyobin <chyobin17@gmail.com>
2023-11-16 10:34:42 +0900

    r

--- model1.py
+++ model1.py
@@ -1,9 +1,12 @@
+from flask import Flask, render_template
 import cv2
 from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
 import time
 import random
+
+app = Flask(__name__)
 
 # Load the model
 model = load_model("keras_Model.h5", compile=False)
@@ -18,9 +21,10 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 cap = cv2.VideoCapture(0)
 
 # Variables for action detection
-start_time = time.time()
 action_interval = 5  # The interval (in seconds) to detect the action
-action_detected = False
+start_time = time.time()
+hints_given = 0
+rounds_played = 0
 
 # Rock-paper-scissors game variables
 user_choice = None
@@ -30,7 +34,38 @@ result = None
 def get_computer_choice():
     return random.choice(["Rock", "Paper", "Scissors"])
 
-while True:
+# Function to provide hints
+def provide_hint():
+    global hints_given
+    hints = [
+        "이 인물은 스포츠 선수입니다.",
+        "이 인물은 올림픽 메달리스트입니다.",
+        "이 인물은 한국의 유명한 가수이기도 합니다.",
+        "이 인물은 배우로서도 활동하고 있습니다.",
+        "이 인물은 대중 매체에 자주 등장하는 연예인입니다."
+    ]
+    if hints_given < len(hints):
+        hint = hints[hints_given]
+        hints_given += 1
+    else:
+        hint = "힌트를 모두 사용했습니다."
+    return hint
+
+# Route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route for the game page
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+# Route for processing game actions
+@app.route('/game/action', methods=['POST'])
+def game_action():
+    global user_choice, computer_choice, result, start_time, action_interval, hints_given, rounds_played, cap
+
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -50,18 +85,11 @@ while True:
     class_name = class_names[index]
     confidence_score = prediction[0][index]
 
-    # Display the frame with prediction
-    cv2.putText(frame, f"Class: {class_name[2:]} - Confidence: {confidence_score:.2f}", (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow('Rock-Paper-Scissors Game', frame)
-
     # Check for action detection
     elapsed_time = time.time() - start_time
-    remaining_time = action_interval - elapsed_time
+    remaining_time = max(0, action_interval - elapsed_time)
 
     if confidence_score > 0.5 and elapsed_time >= action_interval:
-        print(f"Action Detected! Time: {elapsed_time:.2f} seconds")
-        
         # Map class names to rock, paper, or scissors
         if "Rock" in class_name:
             user_choice = "Rock"
@@ -75,28 +103,29 @@ while True:
 
         # Determine the game result
         if user_choice == computer_choice:
-            result = "It's a tie!"
+            result = "무승부입니다!"
         elif (
             (user_choice == "Rock" and computer_choice == "Scissors") or
             (user_choice == "Paper" and computer_choice == "Rock") or
             (user_choice == "Scissors" and computer_choice == "Paper")
         ):
-            result = "You win!"
+            result = "이겼습니다! " + provide_hint()  # Provide a hint upon winning
         else:
-            result = "You lose!"
+            result = "이런~ 지셨군요. 다시 도전해 보세요!"
 
-        # Print and reset variables for the next detection
-        print(f"User Choice: {user_choice}, Computer Choice: {computer_choice}")
-        print(result)
+        # Reset variables for the next detection
         action_detected = True
         start_time = time.time()
+        hints_given = 0
+        rounds_played += 1
 
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Check if 5 rounds have been played
+        if rounds_played == 5:
+            result = "5판이 끝났습니다. 이제 김연아의 인물을 맞춰보세요!"
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    return render_template('game.html', user_choice=user_choice, computer_choice=computer_choice,
+                           result=result, remaining_time=remaining_time)
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
