e66ddc01e4485b21f883dfc5d559d2391759f7dd
chyobin <chyobin17@gmail.com>
2023-11-26 20:44:52 +0900

    r

--- model1.py
+++ model1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import cv2
 from keras.models import load_model
 from PIL import Image, ImageOps
@@ -8,15 +8,27 @@ import random
 
 app = Flask(__name__)
 
-# 모델 로드 및 기타 초기화 코드
+
+
+# 모델 로드
 model = load_model("keras_Model.h5", compile=False)
+
+# UTF-8 인코딩으로 레이블 로드
 class_names = open("labels.txt", "r", encoding="utf-8").readlines()
+
+# keras 모델에 피드할 적절한 모양의 배열 생성
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+# 카메라에 연결 (0은 일반적으로 기본 카메라)
 cap = cv2.VideoCapture(0)
-action_interval = 20
+
+# 행동 감지를 위한 변수
+action_interval = 20  # 행동을 감지할 간격 (초)
 start_time = time.time()
 hints_given = 0
 rounds_played = 0
+
+# 가위바위보 게임 변수
 user_choice = None
 computer_choice = None
 result = None
@@ -24,6 +36,7 @@ result = None
 def get_computer_choice():
     return random.choice(["Rock", "Paper", "Scissors"])
 
+# 힌트 제공 함수
 def provide_hint():
     global hints_given
     hints = [
@@ -34,48 +47,57 @@ def provide_hint():
         "이 인물은 대중 매체에 자주 등장하는 연예인입니다."
     ]
     if hints_given < len(hints):
-        return hints[hints_given]
-    return None
+        print("힌트: " + hints[hints_given])
+        hints_given += 1
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/game_start')
-def game_start():
-    hint = provide_hint()
-    return render_template('game_start.html', hint=hint)
-
-@app.route('/game')
-def game():
-    global cap, start_time, hints_given, rounds_played, user_choice, computer_choice, result
-    
+while True:
+    # 프레임 단위로 캡처
     ret, frame = cap.read()
+
+    # 프레임을 PIL 이미지로 변환
     image = Image.fromarray(frame)
+
+    # 이미지 크기 조절 및 전처리
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
     image_array = np.asarray(image)
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
     data[0] = normalized_image_array
-    
+
+    # 클래스 예측
     prediction = model.predict(data)
     index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
+
+    # 예측과 함께 프레임 표시
+    cv2.putText(frame, f"Class: {class_name[2:]} - Confidence: {confidence_score:.2f}", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
+    # 남은 시간 계산 및 표시
     elapsed_time = time.time() - start_time
     remaining_time = max(0, action_interval - elapsed_time)
+    cv2.putText(frame, f"Remaining Time: {remaining_time:.2f} seconds", (10, 70),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
+    cv2.imshow('Rock-Paper-Scissors Game', frame)
+
+    # 행동 감지 확인
     if confidence_score > 0.5 and elapsed_time >= action_interval:
+        print(f"Action Detected! Time: {elapsed_time:.2f} seconds")
+        
+        # 클래스 이름을 가위, 바위 또는 보로 매핑
         if "Rock" in class_name:
             user_choice = "Rock"
         elif "Paper" in class_name:
             user_choice = "Paper"
         elif "Scissors" in class_name:
             user_choice = "Scissors"
-        
+
+        # 컴퓨터의 선택 얻기
         computer_choice = get_computer_choice()
-        
+
+        # 게임 결과 결정
         if user_choice == computer_choice:
             result = "무승부입니다!"
         elif (
@@ -83,56 +105,54 @@ def game():
             (user_choice == "Paper" and computer_choice == "Rock") or
             (user_choice == "Scissors" and computer_choice == "Paper")
         ):
-            result = "이겼습니다! " + provide_hint()
+            result = "이겼습니다! "
+            provide_hint()  # 이기면 힌트 제공
         else:
             result = "이런~ 지셨군요. 다시 도전해 보세요!"
-        
+
+        # 결과 출력 및 다음 감지를 위한 변수 재설정
+        print(f"User Choice: {user_choice}, Computer Choice: {computer_choice}")
+        print(result)
         action_detected = True
         start_time = time.time()
+        hints_given = 0
         rounds_played += 1
-        
-        if rounds_played == 5:
-            return redirect(url_for('game_over'))
-    
-    return render_template('game.html', frame=frame, class_name=class_name[2:], confidence_score=confidence_score, remaining_time=remaining_time, result=result, user_choice=user_choice, computer_choice=computer_choice)
 
-@app.route('/chat')
-def chat():
-    return render_template('chat.html')
+        # 5판을 다 플레이했는지 확인
+        if rounds_played == 5:
+            print("5판이 끝났습니다. 이제 인물을 맞춰보세요!")
+            break
+
+    # 'q' 키가 눌렸는지 확인하여 루프 종료
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 사용자에게 해당 인물을 추측하도록 요청
+print("기회가 끝났습니다. 이 사람은 누굴까요?")
+user_guess = input("인물의 이름을 입력하세요: ")
+
+# 사용자의 추측이 정확한지 확인
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/play_game', methods=['POST'])
+def play_game():
+    # ... (이전까지의 코드와 동일)
+
+    if rounds_played == 5:
+        return render_template('end_game.html')
+
+    return render_template('play_game.html', result=result, user_choice=user_choice, computer_choice=computer_choice)
 
 @app.route('/guess', methods=['POST'])
 def guess():
     user_guess = request.form['user_guess']
+
     if user_guess == "김연아":
-        return redirect(url_for('game_result', result="정답입니다! 축하합니다!"))
+        return render_template('win.html')
     else:
-        return redirect(url_for('game_result', result="아쉽군요. 다시 도전해 보세요!"))
-
-@app.route('/more_hints')
-def more_hints():
-    hint = provide_hint()
-    return render_template('more_hints.html', hint=hint)
-
-@app.route('/game_records')
-def game_records():
-    # 여기에 게임 결과를 기록하고 보여주는 로직 추가
-    records = ["Game 1: Win", "Game 2: Draw", "Game 3: Loss"]
-    return render_template('game_records.html', records=records)
-
-@app.route('/game_over', methods=['GET', 'POST'])
-def game_over():
-    global rounds_played
-    if request.method == 'POST':
-        user_guess = request.form['user_guess']
-        if user_guess == "김연아":
-            return redirect(url_for('game_result', result="정답입니다! 축하합니다!"))
-        else:
-            return redirect(url_for('game_result', result="아쉽군요. 다시 도전해 보세요!"))
-    return render_template('game_over.html')
-
-@app.route('/game_result/<result>')
-def game_result(result):
-    return render_template('game_result.html', result=result, rounds_played=rounds_played)
+        return render_template('lose.html', correct_answer="김연아")
 
 if __name__ == '__main__':
     app.run(debug=True)
